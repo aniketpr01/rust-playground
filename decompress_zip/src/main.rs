d7c4487b91729934bd17c17985fbef0b47b4ba85
aniket-prajapati <anikethprajapati@gmail.com>
2023-05-07 17:34:05 +0530

    added few examples

---
+++ decompress_zip/src/main.rs
@@ -0,0 +1,54 @@
+use std::fs;
+use std::io;
+
+fn main() {
+    std::process::exit(final_main())
+}
+
+fn final_main() -> i32 {
+    let args: Vec<_> = std::env::args().collect();
+    if args.len() < 2 {
+        println!("Usage: {} <zipfile>", args[0]);
+        return 1;
+    }
+
+    let zipfile = std::path::Path::new(&*args[1]);
+    let file = fs::File::open(&zipfile).unwrap();
+    let mut archive = zip::ZipArchive::new(file).unwrap();
+
+    for i in 0..archive.len() {
+        let mut file = archive.by_index(i).unwrap();
+        let outpath = match file.enclosed_name() {
+            Some(path) => path.to_owned(),
+            None => continue,
+        };
+        {
+            let comment = file.comment();
+            if !comment.is_empty() {
+                println!("File {} comment: {}", i, comment);
+            }
+        }
+        if (&*file.name()).ends_with('/') {
+            println!(
+                "File {} extracted to \"{}\"",
+                i,
+                outpath.as_path().display()
+            );
+            fs::create_dir_all(&outpath).unwrap();
+        } else {
+            println!(
+                "File {} extracted to \"{}\" ({} bytes)",
+                i,
+                outpath.as_path().display(),
+                file.size()
+            );
+            if let Some(p) = outpath.parent() {
+                if !p.exists() {
+                    fs::create_dir_all(&p).unwrap();
+                }
+            }
+            let mut outfile = fs::File::create(&outpath).unwrap();
+            io::copy(&mut file, &mut outfile).unwrap();
+        }
+    }
+}
